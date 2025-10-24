66fbd8c2af2ba0ce5759836cd53e62bd3d1b2639
Sabbir Ahmed Jisan <jisancse875@gmail.com>
2025-10-25 01:07:02 +0600

    Add files via upload

--- 266B-QueueAtSchool.cpp
+++ 266B-QueueAtSchool.cpp
@@ -6,7 +6,7 @@ int main(){
     string s;
     cin>>s;
     for(int i=0; i<t; i++){
-        for(int j=0; j<n-1; j++){
+        for(int j=0; j<n-1; j++){ 
             if(s[j]=='B' && s[j+1]=='G'){
                 s[j]='G', s[j+1]='B';
                 j++;
