0a0d647a30d3f78a87f5d280cc8e3343ca2884f9
Sabbir Ahmed Jisan <jisancse875@gmail.com>
2025-10-29 00:03:15 +0600

    Add files via upload

---
+++ 144A-ArrivalOfGeneral.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int max=0, min=0;
+    for(int i=0; i<n; i++){
+        if(arr[i]>arr[max])
+            max=i;
+        if(arr[i]<arr[min])
+            min=i;
+    }
+    int c=max+(n-1-min);
+    if(min<max)
+        c--;
+    cout<<c;
+}
