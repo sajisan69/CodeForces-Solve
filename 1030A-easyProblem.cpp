f59a547eb3c4e50d4e3f05a472fda96842be46a8
Sabbir Ahmed Jisan <jisancse875@gmail.com>
2025-11-10 23:36:15 +0600

    Add files via upload

---
+++ 1030A-easyProblem.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    for(int i=0; i<n; i++){
+        if(arr[i]==1){
+            cout<<"HARD";
+            return 0;
+        }
+    }
+    cout<<"EASY";
+    return 0;
+}
