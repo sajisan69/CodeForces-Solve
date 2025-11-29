51735172d9cde25b54ece05e3b70322d77fba8fa
Sabbir Ahmed Jisan <jisancse875@gmail.com>
2025-11-29 21:52:46 +0600

    Add files via upload

---
+++ 1742-Sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b==c||b+c==a||a+c==b)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+}
